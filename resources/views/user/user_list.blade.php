cf263c48c2164af21429a79183ba75e84c0aeada
unknown <briansalo1997@gmail.com>
2022-03-04 20:36:19 +0800

    phase 1 User list(function 2)

--- resources/views/user/user_list.blade.php
+++ resources/views/user/user_list.blade.php
@@ -1,8 +1,44 @@
 <x-app-layout>
+
     <x-slot name="header">
        
     </x-slot>
-    userlistpage
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                User List
+                <button type="button" class="btn btn-success" style="float:right">Add User</button>
+            </div>
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                         <tr>
+                            <th>ID Number</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($alluser as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->first_name}} {{$user->last_name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td></td>
+                            <td>
+                                <button type="button" class="btn btn-primary">Edit</button>
+                                <button type="button" class="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>        
+            </div><!--card-body-->
+        </div><!--card-->
+    </div><!--container-->
 
 
 </x-app-layout>
